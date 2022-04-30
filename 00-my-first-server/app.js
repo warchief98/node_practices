a48e7b37f1941036a61261f7ed23927ddb84bb8d
warchief98 <mohseni.amir.eh@gmail.com>
2022-04-30 23:01:47 +0430

    module test for fs

--- 00-my-first-server/app.js
+++ 00-my-first-server/app.js
@@ -1,8 +1,22 @@
 var http = require('http');
 
-//create a server object:
-http.createServer(function(req, res) {
-    res.write('Hello World!'); //write a response to the client
-    res.end(); //end the response
-}).listen(8080); //the server object listens on port 8080
-//just some changes
+const fs = require('fs');
+
+const port = '8000'
+
+fs.readFile('index.html', (err, html) => {
+    if (err) throw err;
+    else {
+        //create a server object:
+        const server = http.createServer(function(req, res) {
+            res.writeHead(200, { 'content-type': 'text/html' });
+            res.write(req.url); //write a response to the client
+            res.write(html);
+            res.end(); //end the response
+        })
+
+        server.listen(port, () => {
+            console.log('server is available on Port: ' + port)
+        })
+    }
+})
---
+++ 01-module-test/02-fs.js
@@ -0,0 +1,5 @@
+const fs = require('fs');
+
+fs.unlink('newAppendFile', () => {
+    console.log('sadeletedved')
+})
