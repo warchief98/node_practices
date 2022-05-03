065e9e1b4c65ea099b60fec028dbbd78a94f46ef
warchief98 <mohseni.amir.eh@gmail.com>
2022-05-03 21:13:10 +0430

    add 2 file on game=1 project on nodejs server

--- 07-game-1/app.js
+++ 07-game-1/app.js
@@ -1,1 +1,34 @@
 const actions = require('./js/actions');
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+
+const Port = "9000";
+const Host = null;
+
+const server = http.createServer((req, res) => {
+
+
+    fs.readFile("index.html", (err, html) => {
+        if (err) throw err;
+        else {
+            res.writeHead(200, { 'content-type': 'text/html' });
+            res.write(html);
+            res.end();
+        }
+    });
+
+    fs.readFile('./css/main-style.css', (err, css) => {
+        if (err) throw err;
+        res.writeHead(200, { 'content-type': 'text/css' });
+        res.write(css);
+        res.end();
+    });
+
+})
+
+server.listen(Port, Host, (err) => {
+    if (err) throw err;
+    console.log('server is run on port: ' + Port)
+})
